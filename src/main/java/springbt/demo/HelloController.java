d61cac0c75a7736135137cbd84e2956a781e4c0b
unknown <279290309@qq.com>
2020-03-13 19:51:42 +0800

    通过访问url路径是否也能修改mysql数据

--- src/main/java/springbt/demo/HelloController.java
+++ src/main/java/springbt/demo/HelloController.java
@@ -1,13 +1,30 @@
 package springbt.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    /**
+     * 测试访问hello能否修改数据库数据
+     * @return
+     */
     @RequestMapping("/hello")
     public String hello(){
-        return "hello word";
+        String sql="update c_tt set name=? where id=3";
+        int count=jdbcTemplate.update(sql,"kdd3");
+        System.out.print(count);
+        return "333";
     }
 }
